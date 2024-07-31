ec6a2e6d4d318a62a4533ed2cc4a4b3aa9884e2e
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-07-31 10:43:14 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'max-lines-per-function': ['warn', 100],
+    'no-multi-spaces': ['error']
+  },
+}
