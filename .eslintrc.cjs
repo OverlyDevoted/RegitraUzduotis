07a5ee9bf03243d1ad497236b733e6a576051028
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2024-07-31 13:03:03 +0300

    🦴 chore: path alias eslint integration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,13 +10,25 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react', 'react-refresh', '@typescript-eslint', 'prettier'],
+  plugins: ['react', 'react-refresh', '@typescript-eslint', 'prettier', 'import'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'max-lines-per-function': ['warn', 100],
     'no-multi-spaces': ['error'],
+    'import/no-unresolved': 'error',
   },
-}
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: './tsconfig.json',
+      },
+    },
+  },
+};
